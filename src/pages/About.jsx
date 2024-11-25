987907a09684fd122f39f5cc1a714bce4b073cb6
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-25 10:24:56 +0100

    upgrade live commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,6 +16,7 @@ export default function About() {
                 <h1>
                     Home Page
                 </h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, beatae sapiente. Nostrum, delectus in maxime beatae minus pariatur dicta dolorum eveniet repellat, animi consequatur tempora asperiores, fuga voluptate itaque facere?</p>
             </main>
 
             <footer>
